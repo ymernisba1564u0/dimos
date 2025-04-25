9eeb958b67054a76b11053c73187d8b80d4957ab
lesh <lesh@sysphere.org>
2025-04-24 18:08:15 -0700

    more smudge
    Former-commit-id: 04d50501281dfe2697cdf3d3d40d753ef7077ac9

--- dimos/robot/global_planner/planner.py
+++ dimos/robot/global_planner/planner.py
@@ -41,7 +41,7 @@ class Planner(Visualizable):
             logger.warning("No path found to the goal.")
             return False
 
-        return self.local_nav(path.resample(1.0), stop_event=stop_event)
+        return self.local_nav(path, stop_event=stop_event)
 
 
 class AstarPlanner(Planner):
@@ -58,7 +58,7 @@ class AstarPlanner(Planner):
     def plan(self, goal: VectorLike) -> Path:
         [pos, rot] = self.base_link()
         costmap = self.costmap()
-        smudge = costmap.smudge()
+        smudge = costmap.smudge(iterations=3)
 
         self.vis("planner_costmap", smudge)
         self.vis("target", goal)
