3c383fafbc842c79fd90484f80a5369b8e4d0066
mustafab0 <mustafabhadsorawala@gmail.com>
2025-12-25 18:51:51 +0000

    added custom messages for robot state and joint commands
    Former-commit-id: 00bc33169617251a5ba33374c3238ed01d6d6b64 [formerly 274d0d79d27290ef1db17d16a4757cffac729975] [formerly 7b3489e1df1bb08df7c779655b3054acfa085cdb [formerly f94d6e50d246cff9417475cfce6acf4518364f80]]
    Former-commit-id: c9f5130f6517de30c65b8d8ce5bed08f481f540e
    Former-commit-id: e0e86dfbf2878321a6e88340b1d59a9eb27c9c14

--- dimos/msgs/sensor_msgs/__init__.py
+++ dimos/msgs/sensor_msgs/__init__.py
@@ -5,3 +5,5 @@ from dimos.msgs.sensor_msgs.PointCloud2 import PointCloud2
 
 __all__ = ["CameraInfo", "Image", "ImageFormat", "Joy", "PointCloud2"]
 from dimos.msgs.sensor_msgs.JointState import JointState
+from dimos.msgs.sensor_msgs.RobotState import RobotState
+from dimos.msgs.sensor_msgs.JointCommand import JointCommand
