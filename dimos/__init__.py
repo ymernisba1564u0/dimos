369c1e387311557341c091291899a527a448d421
Ymer Nisbau <ymernisba1564u0@users.noreply.github.com>
2026-04-21 14:31:25 +0800

    feat: add dimos package init with version metadata

---
+++ dimos/__init__.py
@@ -0,0 +1,21 @@
+"""dimos - A fork of dimensionalOS/dimos.
+
+A framework for building dimensional, reactive, and composable
+robotics and agent systems with RxPY-based pipelines.
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("dimos")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
+
+__author__ = "dimos contributors"
+__license__ = "Apache-2.0"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__license__",
+]
